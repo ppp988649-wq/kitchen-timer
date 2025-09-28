af4bae22d0b8673f75ca9bed2f998cf4bb416f41
ppp988649-wq <ppp988649@gmail.com>
2025-09-28 16:26:21 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'kitchen-timer-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-1024.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
